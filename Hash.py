c958420b4992ec1da4c68dcd8123721a2f57e17d
Cynthia Castillo <ca.castillo@gmail.com>
2022-03-18 08:59:47 -0600

    Agregar colision

--- Hash.py
+++ Hash.py
@@ -1,19 +1,40 @@
+from node import Node
+
 class Hash:
     def __init__( self ):
         self.data = []
         self.capacidad = 5
         for i in range( 0, self.capacidad + 1 ):
             self.data.append( None )
+    #Cynthia Data[0] ->Cynthia.next = None
+    #Vale INDICE -> 0
+    #nuevoNodo = Vale
 
-    def agrega( self, num ):
+    #aux = Cynthia
+    #Cynthia.next = Vale
+
+    #Juan
+    #nuevoNodo = Juan
+    #indice = 0
+    #aux = Cynthia
+    #aux = Cynthia.next -> Vale
+    #Vale.next = Juan
+
+    #data[0] = Cynthia Cynthia.next=Vale; Vale Vale.next= Juan; Juan
+
+    def agrega( self, nuevoNodo ):
         #Quiero guardar el num 10
-        indice = int(int((num * 13 + 7) / 3) * 11 / 5 ) % self.capacidad #regresa un número entre 0 y mi capacidad
+        indice = int(int((nuevoNodo.identificador * 13 + 7) / 3) * 11 / 5 ) % self.capacidad #regresa un número entre 0 y mi capacidad
         #Agregar el nodo en DATA
         if self.data[indice] == None:
-            self.data[indice] = num
-            print("El número ",num, " se guardará en el índice ", indice)
+            self.data[indice] = nuevoNodo
+            print("El nodo de ",nuevoNodo.nombre, " se guardará en el índice ", indice)
         else:
-            print("El índice ", indice, " para el numero ",num," no está disponible. Hay que ajustar la lista")
+            aux = self.data[indice]
+            while aux.next != None:
+                aux = aux.next
+            aux.next = nuevoNodo
+            print("El nodo de ", nuevoNodo.nombre, " se agregó en el índice ",indice,". Junto a ",aux.nombre)
         
     def buscaElemento( self, num ):
         #recibo el 13
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from Hash import Hash
+from node import Node
 
 tablaNumeros = Hash()
 
-tablaNumeros.agrega( 13 )
-tablaNumeros.agrega( 72 ) #25 por lo pronto no se guarda
-tablaNumeros.agrega( 91 ) #100 NO se guarda
-tablaNumeros.agrega( 8 )
-tablaNumeros.agrega( 10 )
+persona1 = Node("Cynthia", "Castillo", 123)
+persona2 = Node("Valeria", "Romero", 456)
+persona3 = Node("Juan", "Perez", 789)
+
+tablaNumeros.agrega(persona1)
+tablaNumeros.agrega(persona2)
+tablaNumeros.agrega(persona3)
 
 # data[0] = 72
 # data[1] = 91
@@ -14,9 +17,9 @@ tablaNumeros.agrega( 10 )
 # data[3] = None
 # data[4] = 10
 
-print("---------")
+# print("---------")
 
-tablaNumeros.buscaElemento( 13 )
-tablaNumeros.buscaElemento( 10 ) 
-tablaNumeros.buscaElemento( 91 ) 
-tablaNumeros.buscaElemento( 2 )
+# tablaNumeros.buscaElemento( 13 )
+# tablaNumeros.buscaElemento( 10 ) 
+# tablaNumeros.buscaElemento( 91 ) 
+# tablaNumeros.buscaElemento( 2 )
